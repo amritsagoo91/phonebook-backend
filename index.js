960d9e0dad60ddf33182bf46107e4c9c541cafe9
amritsagoo91 <amritsagoo91@gmail.com>
2024-12-03 17:43:37 -0800

    3.9 Phonebook backend step 9

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
 const app = express();
 
+
+
 app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('body', (req, res) => {
     if (req.method === 'POST') {
@@ -87,7 +92,7 @@ app.delete('/api/persons/:id', (req, res) => {
 })
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running in port ${PORT}`)
 })
